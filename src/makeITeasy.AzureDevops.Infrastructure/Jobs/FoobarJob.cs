d4a217453b8efae09bc66ebb740ad05167f4093c
Manu D <edesigau@hotmail.fr>
2020-05-17 08:53:02 +0200

    projet setup - initial commit

---
+++ src/makeITeasy.AzureDevops.Infrastructure/Jobs/FoobarJob.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace makeITeasy.AzureDevops.Infrastructure.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class FoobarJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
